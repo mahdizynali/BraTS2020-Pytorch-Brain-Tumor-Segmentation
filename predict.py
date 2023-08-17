f9e79d0f28a5df47ba32acfd8abbaafc89cdd75c
mahdizynali <zeinali.mahdy@.com>
2023-08-17 16:24:14 +0330

    add prediction

--- predict.py
+++ predict.py
@@ -1,13 +1,28 @@
 from config import *
+from dataGenerator import extractPath, generator
+from model import *
+from coefficent import *
+from train import Trainer
+from model import Modified3DUNet
 
-if config.pretrained_model_path is not None:
-    trainer.load_predtrain_model(config.pretrained_model_path)
-    
-    # if need - load the logs.      
-    train_logs = pd.read_csv(config.train_logs_path)
-    trainer.losses["train"] =  train_logs.loc[:, "train_loss"].to_list()
-    trainer.losses["val"] =  train_logs.loc[:, "val_loss"].to_list()
-    trainer.dice_scores["train"] = train_logs.loc[:, "train_dice"].to_list()
-    trainer.dice_scores["val"] = train_logs.loc[:, "val_dice"].to_list()
-    trainer.jaccard_scores["train"] = train_logs.loc[:, "train_jaccard"].to_list()
-    trainer.jaccard_scores["val"] = train_logs.loc[:, "val_jaccard"].to_list()
+model = Modified3DUNet()
+sum([param.nelement() for param in model.parameters()])
+
+pred = Trainer(net=model,
+                  fold=0,
+                  dataset=generator,
+                  criterion=BCEDiceLoss(),
+                  lr=configuration.learnin_rate,
+                  accumulation_steps=configuration.acc_steps,
+                  batch_size = configuration.batch_size,
+                  num_epochs = configuration.epochs,
+                  path_to_csv = configuration.train_csv_path)
+
+pred.load_predtrain_model(configuration.pretrained_model_path)
+train_logs = pd.read_csv(configuration.train_logs_path)
+pred.losses["train"] =  train_logs.loc[:, "train_loss"].to_list()
+pred.losses["val"] =  train_logs.loc[:, "val_loss"].to_list()
+pred.dice_scores["train"] = train_logs.loc[:, "train_dice"].to_list()
+pred.dice_scores["val"] = train_logs.loc[:, "val_dice"].to_list()
+pred.jaccard_scores["train"] = train_logs.loc[:, "train_jaccard"].to_list()
+pred.jaccard_scores["val"] = train_logs.loc[:, "val_jaccard"].to_list()
--- train.py
+++ train.py
@@ -7,11 +7,8 @@ data = extractPath()
 train_df, val_df, test_df = data.train_test_valid()
 # data.plotResult(train_df, val_df, test_df)
 
-def data_loader(dataset: torch.utils.data.Dataset,
-                   path_to_csv, phase, fold=0,
-                   batch_size=configuration.batch_size,
-                   num_workers=configuration.num_workers):
-    # apelam dataloader-ul pentru antrenarea modelului
+def data_loader(dataset: torch.utils.data.Dataset,path_to_csv, phase, fold=0):
+
     df = pd.read_csv(path_to_csv)
     
     train_df = df.loc[df['fold'] != fold].reset_index(drop=True) # fold mentions Kfold method
@@ -28,25 +25,22 @@ def data_loader(dataset: torch.utils.data.Dataset,
     )
     return dataloader
 
-dataloader = data_loader(dataset=generator, path_to_csv=configuration.train_csv_path, phase='valid', fold=0)
-
-
+# dataloader = data_loader(dataset=generator, path_to_csv=configuration.train_csv_path, phase='valid', fold=0)
 
 class Trainer:
     def __init__(self, net: nn.Module, dataset: torch.utils.data.Dataset, criterion: nn.Module,
-                 lr, accumulation_steps, batch_size, fold, num_epochs, path_to_csv, display_plot = True):
+                 lr, accumulation_steps, batch_size, fold, num_epochs, path_to_csv, display_plot = False):
 
-        print("Try to start processing ...\n")
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        print(f"Try to start processing on {self.device} ...\n")
         self.display_plot = display_plot
         self.net = net
         self.net = self.net.to(self.device)
         self.criterion = criterion
         self.optimizer = Adam(self.net.parameters(), lr=lr)
-        self.scheduler = ReduceLROnPlateau(self.optimizer, mode="min",
-                                           patience=4, verbose=True)
+        self.scheduler = ReduceLROnPlateau(self.optimizer, mode="min", patience=4, verbose=True)
         self.accumulation_steps = accumulation_steps // batch_size
-        self.phases = ["train", "val"]
+        self.phases = ["train", "valid"]
         self.num_epochs = num_epochs
 
         self.dataloaders = {
@@ -55,8 +49,6 @@ class Trainer:
                 path_to_csv = path_to_csv,
                 phase = phase,
                 fold = fold,
-                batch_size = batch_size,
-                num_workers = configuration.num_workers
             )
             for phase in self.phases
         }
@@ -68,7 +60,7 @@ class Trainer:
         self.spf_scores = {phase: [] for phase in self.phases}
 
          
-    def _compute_loss_and_outputs(self,
+    def compute_loss_and_outputs(self,
                                   images: torch.Tensor,
                                   targets: torch.Tensor):
         images = images.to(self.device)
@@ -77,7 +69,7 @@ class Trainer:
         loss = self.criterion(logits, targets)
         return loss, logits
 
-    def _do_epoch(self, epoch: int, phase: str):
+    def do_epoch(self, epoch: int, phase: str):
         print("#"*50)
         t = time.process_time()
         print(f"---------- {phase} initial mode epoch {epoch + 1} ----------\n")
@@ -93,7 +85,7 @@ class Trainer:
         for itr, data_batch in tqdm(enumerate(dataloader), total=len(dataloader), desc="Steps"):    
             counter += 1
             images, targets = data_batch['image'], data_batch['mask']
-            loss, logits = self._compute_loss_and_outputs(images, targets)
+            loss, logits = self.compute_loss_and_outputs(images, targets)
             loss = loss / self.accumulation_steps
             if phase == "train":
                 loss.backward()
@@ -101,15 +93,13 @@ class Trainer:
                     self.optimizer.step()
                     self.optimizer.zero_grad()
             running_loss += loss.item()
-            meter.update(logits.detach().cpu(),
-                         targets.detach().cpu()
-                        )
+            meter.update(logits.detach().cpu(), targets.detach().cpu())
             
         epoch_loss = (running_loss * self.accumulation_steps) / total_batches
         epoch_dice, epoch_iou, epoch_sen, epoch_spf  = meter.get_metrics()
         elapsed_time = time.process_time() - t
 
-        print(f"loss : {epoch_loss} \ndice : {epoch_dice}")
+        print(f"loss : {epoch_loss} \ndice : {epoch_dice}\nIoU : {epoch_iou}")
         print(f"epoch time : {elapsed_time:.2f} sec\n")
         
         self.losses[phase].append(epoch_loss)
@@ -119,18 +109,18 @@ class Trainer:
         self.spf_scores[phase].append(epoch_spf)
         return epoch_loss
             
-    def _plot_train_history(self):
+    def plot_train_history(self):
         data = [self.losses, self.dice_scores]
         colors = ['deepskyblue', "crimson"]
         labels = [
             f"""
             train loss {self.losses['train'][-1]}
-            val loss {self.losses['val'][-1]}
+            val loss {self.losses['valid'][-1]}
             """,
             
             f"""
             train dice score {self.dice_scores['train'][-1]}
-            val dice score {self.dice_scores['val'][-1]} 
+            val dice score {self.dice_scores['valid'][-1]} 
             """, 
             
         ]
@@ -138,36 +128,35 @@ class Trainer:
         with plt.style.context("seaborn-dark-palette"):
             fig, axes = plt.subplots(2, 1, figsize=(8, 10))
             for i, ax in enumerate(axes):
-                ax.plot(data[i]['val'], c=colors[0], label="val")
+                ax.plot(data[i]['valid'], c=colors[0], label="valid")
                 ax.plot(data[i]['train'], c=colors[-1], label="train")
                 ax.set_title(labels[i])
                 ax.legend(loc="upper right")
                 
             plt.tight_layout()
-            # plt.show()
+            plt.show()
 
     def setup(self):
         for epoch in range(self.num_epochs):
-            self._do_epoch(epoch, "train")
+            self.do_epoch(epoch, "train")
             with torch.no_grad():
-                val_loss = self._do_epoch(epoch, "val")
+                val_loss = self.do_epoch(epoch, "valid")
                 self.scheduler.step(val_loss)
             if self.display_plot:
-                self._plot_train_history()
+                self.plot_train_history()
                 
             if val_loss < self.best_loss:
-                print(f"\nSaved checkpoint epoch {epoch + 1}\n{'#'*50}\n")
+                print(f"\nSaved new best checkpoint epoch {epoch + 1}\n{'#'*50}\n")
                 self.best_loss = val_loss
                 torch.save(self.net.state_dict(), "trainResult/best_model.pth")
             print()
-        self._save_train_history()
+        self.save_train_history()
             
-    def load_predtrain_model(self,
-                             state_path: str):
+    def load_predtrain_model(self, state_path: str):
         self.net.load_state_dict(torch.load(state_path))
         print("Predtrain model loaded")
         
-    def _save_train_history(self):
+    def save_train_history(self):
         torch.save(self.net.state_dict(),
                    f"trainResult/last_epoch_model.pth")
 
@@ -207,16 +196,15 @@ nodel=Modified3DUNet()
 # print(nodel)
 sum([param.nelement() for param in nodel.parameters()])
 
+def run() :
 
-
-train = Trainer(net=nodel,
-                  fold=0,
-                  dataset=generator,
-                  criterion=BCEDiceLoss(),
-                  lr=configuration.learnin_rate,
-                  accumulation_steps=configuration.acc_steps,
-                  batch_size = configuration.batch_size,
-                  num_epochs = configuration.epochs,
-                  path_to_csv = configuration.train_csv_path)
-
-train.setup()
+    trainer = Trainer(net=nodel,
+                    fold=0,
+                    dataset=generator,
+                    criterion=BCEDiceLoss(),
+                    lr=configuration.learnin_rate,
+                    accumulation_steps=configuration.acc_steps,
+                    batch_size = configuration.batch_size,
+                    num_epochs = configuration.epochs,
+                    path_to_csv = configuration.train_csv_path)
+    trainer.setup()
